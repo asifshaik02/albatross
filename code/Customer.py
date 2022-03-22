214bdc71d8b7b9303ac4482b82d04792ea745fd9
keertangh <keertangh123@gmail.com>
2022-03-22 17:00:58 +0530

    class files

---
+++ code/Customer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Customer:
+    def __init__(self):
+        self.c_name = None
+        self.c_phone = None
+
---
+++ code/Owner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Owner:
+    def __init__(self):
+        self.o_name = None
+        self.o_id = None
+        self.o_phone = None
+        self.password = None
+
+    def login(self, id, pass):
+        pass
+
+    def getEmployeeDetails(self, ):
+        pass
+
+    def getMedicineDetails(self, ):
+        pass
+
+    def getBillTransactions(self, ):
+        pass
+
