24693fae1638ad3d7abdafd377db3f3dd7dfcc26
asifshaik02 <asifmohammed290602@gmail.com>
2022-03-22 23:23:54 +0530

    constructors for employee and medicine

--- code/Employee.py
+++ code/Employee.py
@@ -2,16 +2,16 @@
 #-*- coding: utf-8 -*-
 
 class Employee:
-    def __init__(self):
-        self.e_name = None
-        self.e_id = None
-        self.password = None
-        self.e_phone = None
-        self.role = None
+    def __init__(self,e_name,e_id,passw,e_phone,role):
+        self.e_name = e_name
+        self.e_id = e_id
+        self.password = passw
+        self.e_phone = e_phone
+        self.role = role
 
-    def login(self, id, pass):
+    def login(self, id, passw):
         pass
 
-    def billgeneration(self, ):
+    def billgeneration(self, mIdArr):
         pass
 
--- code/Medicine.py
+++ code/Medicine.py
@@ -2,28 +2,28 @@
 #-*- coding: utf-8 -*-
 
 class Medicine:
-    def __init__(self):
-        self.m_name = None
-        self.m_id = None
-        self.mfg = None
-        self.type = None
-        self.rack_no = None
-        self.cost = None
-        self.exp_date = None
-        self.reorder_lvl = None
+    def __init__(self,m_name,m_id,mfg,t,rack_no,cost,exp_date,reorder_lvl):
+        self.m_name = m_name
+        self.m_id = m_id
+        self.mfg = mfg
+        self.type = t
+        self.rack_no = rack_no
+        self.cost = cost
+        self.exp_date = exp_date
+        self.reorder_lvl = reorder_lvl
 
-    def getRackNo(self, ):
+    def getRackNo(self, m_id):
         pass
 
-    def getType(self, ):
+    def getType(self, m_id):
         pass
 
-    def expiryStatus(self, ):
+    def expiryStatus(self, m_id):
         pass
 
-    def getCost(self, ):
+    def getCost(self, m_id):
         pass
 
-    def getReorderLevel(self, ):
+    def getReorderLevel(self, m_id):
         pass
 
