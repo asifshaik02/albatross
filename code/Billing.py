9d157a562cc274a36321d5abeb4b04aad2754ff1
Anil Thogarcheti <53226949+ThogarchetiAnil@users.noreply.github.com>
2022-03-22 23:14:16 +0530

    Update Billing.py

--- code/Billing.py
+++ code/Billing.py
@@ -2,12 +2,12 @@
 #-*- coding: utf-8 -*-
 
 class Billing:
-    def __init__(self):
-        self.b_id = None
-        self.billDate = None
-        self.b_cost = None
-        self.quantity = None
-        self.medicineName = None
+    def __init__(self,bid,b_date,cost,quantity,m_name):
+        self.b_id = bid
+        self.billDate = b_date
+        self.b_cost = cost
+        self.quantity = quantity
+        self.medicineName = m_name
 
     def getBillTransactions(self, ):
         pass
