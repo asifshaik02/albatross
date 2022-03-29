61d48eb86cbb73ce8f89ec62a7a7a707508fbe36
asifshaik02 <asifmohammed290602@gmail.com>
2022-03-29 23:51:38 +0530

    update Medicine.py

--- code/Medicine.py
+++ code/Medicine.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+from cgitb import text
+from functools import partial
+from tkinter import *
+
 class Medicine:
-    def __init__(self,m_name,m_id,mfg,t,rack_no,cost,exp_date,reorder_lvl):
+    def __init__(self,m_name=None,m_id=None,mfg=None,t=None,rack_no=None,cost=None,exp_date=None,reorder_lvl=None):
         self.m_name = m_name
         self.m_id = m_id
         self.mfg = mfg
@@ -11,19 +15,80 @@ class Medicine:
         self.cost = cost
         self.exp_date = exp_date
         self.reorder_lvl = reorder_lvl
+        self.gui()
 
     def getRackNo(self, m_id):
-        pass
+        # implemented later using database
+        return "10"
 
     def getType(self, m_id):
-        pass
+        # implemented later using database
+        return "Tablets"
 
     def expiryStatus(self, m_id):
-        pass
+        # implemented later using database
+        return "10/12/2022"
 
     def getCost(self, m_id):
-        pass
+        # implemented later using database
+        return "1000/-"
 
     def getReorderLevel(self, m_id):
-        pass
+        # implemented later using database
+        return "200"
 
+    def getDetails(self,m_id):
+        self.m_id = m_id.get()
+        self.rack_no = self.getRackNo(self.m_id)
+        self.type = self.getType(self.m_id)
+        self.exp_date = self.expiryStatus(self.m_id)
+        self.cost = self.getCost(self.m_id)
+        self.reorder_lvl = self.getReorderLevel(self.m_id)
+
+        
+
+    def gui(self):
+        root = Tk()
+        root.geometry('600x450')  
+        root.title('Medicine')
+
+        topFrame = Frame(root)
+        topFrame.pack()
+
+        medIdLabel = Label(topFrame,text="Medicine Id:",pady=40)
+        medIdLabel.grid(row=0,column=0)
+
+        med_id = StringVar()
+        medIdEntry = Entry(topFrame, textvariable=med_id)
+        medIdEntry.grid(row=0, column=1)
+
+        emptyLabel = Label(topFrame,text="")
+        emptyLabel.grid(row=0,column=2)
+
+        bottomFrame = Frame(root,pady=20)
+        bottomFrame.pack()
+
+        def show(id):
+            self.getDetails(id)
+            Label(bottomFrame,text="Id:").grid(row=0,column=0)
+            Label(bottomFrame,text=self.m_id).grid(row=0,column=1)
+            Label(bottomFrame,text="Rack:").grid(row=1,column=0)
+            Label(bottomFrame,text=self.rack_no).grid(row=1,column=1)
+            Label(bottomFrame,text="Type:").grid(row=2,column=0)
+            Label(bottomFrame,text=self.type).grid(row=2,column=1)
+            Label(bottomFrame,text="Expiry:").grid(row=3,column=0)
+            Label(bottomFrame,text=self.exp_date).grid(row=3,column=1)
+            Label(bottomFrame,text="Cost:").grid(row=4,column=0)
+            Label(bottomFrame,text=self.cost).grid(row=4,column=1)
+            Label(bottomFrame,text="Reorder level:").grid(row=5,column=0)
+            Label(bottomFrame,text=self.reorder_lvl).grid(row=5,column=1)
+
+        getDetails = partial(show, med_id)
+        getRackButton = Button(topFrame,text="Medicine Details", command=getDetails)
+        getRackButton.grid(row=1,column=1)
+        
+
+        root.mainloop()
+
+
+obj = Medicine()
